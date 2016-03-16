0a966a994e72d1824c7ebd77489c7c92f1ca3dca
MatthewAllanWilson <mattwils@gmail.com>
2016-03-16 09:57:56 -0700

    more java questions

--- app.js
+++ app.js
@@ -6,22 +6,49 @@
 
 var userName = prompt('Hello, friend!  Tell me your name so I don\'t have to keep calling you \'friend\'!');
 console.log('User inputs their name.');
-
-alert('Okay, ' + userName + '! I\'m going to ask you some questions so you can get to know me.');
-console.log('Telling user what we will be doing.');
+  alert('Okay, ' + userName + '! I\'m going to ask you some questions so you can get to know me.');
+    console.log('Telling user what we will be doing.');
 
 var fromSeattle = prompt('Did I grow up in Seattle?  Yes or no?');
-if (fromSeattle.toUpperCase() === 'YES') {
-  alert('That is correct!  Although technically I grew up in Mill Creek, about 20 miles North of Seattle.');
+  if (fromSeattle.toUpperCase() === 'YES') {
+    alert('That is correct!  Although technically I grew up in Mill Creek, about 20 miles North of Seattle.');
 } else {
-  alert('Incorrect!  I grew up in Mill Creek, 20 miles North of downtown Seattle.  But I still say I grew up in Seattle!!');
+    alert('Incorrect!  I grew up in Mill Creek, 20 miles North of downtown Seattle.  But I still say I grew up in Seattle!!');
 }
-console.log('User guesses if I grew up in Seattle or not.')
+    console.log('User guesses if I grew up in Seattle or not.');
 
 var inLosAngeles = prompt('Did I live in Los Angeles?  Yes or no?');
-if (inLosAngeles.toUpperCase() === 'YES'){
-  alert ('You are right!  I lived there for 15 years.');
+  if (inLosAngeles.toUpperCase() === 'YES'){
+    alert ('You are right!  I lived there for 15 years.');
 } else {
-  alert('Nope!  I lived in the City of Angels for 15 years.');
+    alert('Nope!  I lived in the City of Angels for 15 years.');
 }
-console.log('User guesses if I lived in Los Angeles.');
+    console.log('User guesses if I lived in Los Angeles.');
+
+var likesSushi = prompt('Do I like Sushi? Yes or no?');
+  if (likesSushi.toUpperCase() === 'YES') {
+    alert('Incorrect!  Even though everyone else in LA loved it, I never caught the Sushi craze.');
+} else {
+    alert('That is right, ' + userName + '!  Even though I lived in the land of Sushi, I was never convinced.');
+}
+
+  var playsPiano = prompt('Do I play piano?  Yes or no?');
+    if (playsPiano.toUpperCase() === 'YES') {
+        alert('Right you are, ' + userName + '! I started playing piano when I was 30.');
+  } else {
+      alert('Incorrect!  I started playing piano when I turned 30.');
+    }
+
+  var likesGolf = prompt('Do I like Golf?  Yes or no?');
+    if (likesGolf.toUpperCase () === 'YES') {
+
+      var whyGolf = prompt('Incorrect!  Do you think all people in LA play golf?  Yes or no?');
+        if (whyGolf.toUpperCase() === 'YES') {
+          alert('Well you are wrong about that!  When I was there I only knew one person who could afford it.');
+        } else {
+          alert('I see.  But you thought there was something golfy about me?  Well, now you know that is not right!');
+        }
+
+  } else {
+    alert('You are right!  I am not a golf guy.');
+  }
